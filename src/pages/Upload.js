23a7b213432bef8b63bfdb6562390909efe3aefc
Marius <marius@myidea.fr>
2022-02-02 11:47:18 +0100

    add dropzone

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,10 +1,81 @@
+import axios from 'axios'
+import { useMemo } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { useState } from 'react/cjs/react.development'
+
+const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  outline: 'none',
+  transition: 'border .24s ease-in-out'
+}
+
+const focusedStyle = {
+  borderColor: '#2196f3'
+}
+
+const acceptStyle = {
+  borderColor: '#00e676'
+}
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+}
+
 function Upload () {
+  const [uploadedImage, setUploadedImage] = useState(null)
+
+  const handleDrop = async (acceptedFiles) => {
+    console.log(acceptedFiles)
+    const data = new FormData()
+    data.append('file', acceptedFiles[0])
+    data.append('file-name', acceptedFiles[0].name)
+
+    const response = await axios.post('http://localhost:4000/upload', data)
+    console.log(response)
+    setUploadedImage(response.data)
+  }
+
+  const {
+    getRootProps,
+    getInputProps,
+    isFocused,
+    isDragAccept,
+    isDragReject
+  } = useDropzone({ accept: 'image/*', onDrop: handleDrop, multiple: false })
+
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isFocused ? focusedStyle : {}),
+    ...(isDragAccept ? acceptStyle : {}),
+    ...(isDragReject ? rejectStyle : {})
+  }), [
+    isFocused,
+    isDragAccept,
+    isDragReject
+  ])
+
   return (
     <div>
-      <form action='http://localhost:4000/upload' method='post' encType='multipart/form-data'>
-        <input type='file' id='file-upload' name='file-upload' />
-        <input type='submit' value='Envoyer' />
-      </form>
+      {
+        uploadedImage
+          ? <img src={`http://localhost:4000/${uploadedImage.path}`} width={400} />
+          : (
+            <div {...getRootProps({ style })}>
+              <input {...getInputProps()} />
+              <p>Glissez-déposez vos fichiers ici</p>
+            </div>
+            )
+        }
     </div>
   )
 }
